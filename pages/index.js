19df84b15f8750f4cb4dbeaa4b389bd98293b664
ARSENLUPIN <mdoukali007@gmail.com>
2021-08-16 02:11:47 +0100

    navbar___

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SectionLogin from '../components/Molecules/SectionLogin';
+import Navb from '../components/Atomes/navbar';
 
 export default function Home() {
   return (
@@ -12,7 +13,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>       
+      <main className={styles.main}> 
+      <div className="container-fluid">
+        <Navb/>
+        </div>      
         <div className="container">
           <div className="row">
             <SectionLogin />
