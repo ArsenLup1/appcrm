08b149182fd41fc65a094cb6e5d173d3d7e0e0cd
ARSENLUPIN <mdoukali007@gmail.com>
2021-08-17 16:43:09 +0100

    TewteCommit

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SectionLogin from '../components/Molecules/SectionLogin';
-import Navb from '../components/Atomes/navbar';
 
 export default function Home() {
   return (
