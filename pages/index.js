96344961ddefff077f08bcaaa7312db80ea90200
ArsenLup1 <87782703+ArsenLup1@users.noreply.github.com>
2021-08-31 19:11:17 +0100

    change--index

--- pages/index.js
+++ pages/index.js
@@ -17,14 +17,14 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        <Link href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer">
           Copyright ComJob 2021 {' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
-        </a>
+        </Link>
       </footer>
     </div>
   )
