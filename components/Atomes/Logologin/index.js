97a43c0dca8aa15d2b2915082ae36805d71e2702
ARSENLUPIN <mdoukali007@gmail.com>
2021-08-31 11:51:59 +0100

    Retest

--- components/Atomes/Logologin/index.js
+++ components/Atomes/Logologin/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LogologinStc from "./logologin.stc";
+import Image from 'next/image';
 
 const LogoLogin = () => {
 
     return ( 
         <LogologinStc>
             <div className="col-lg-12 pt-5 mb-1 align-middle d-flex justify-content-center login">
-              <img src="./img/Logo__ComJ.png" className="rounded thumbnail monlogo" alt="logo" width="300px" />
+              <Image src="/img/Logo__ComJ.png" alt="Vercel Logo" width={350} height={306} />
+              {/*<img src="./img/Logo__ComJ.png" className="rounded thumbnail monlogo" alt="logo" width="300px" />*/}
             </div>
         </LogologinStc>
 
